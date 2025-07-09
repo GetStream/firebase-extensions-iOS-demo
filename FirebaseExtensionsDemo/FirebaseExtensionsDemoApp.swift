fd0cccb7b299feaade180e8cbf1aad0477c9aa25
Stefan Blos <stefan.blos@gmail.com>
2025-07-09 17:10:07 +0200

    Initial Commit

---
+++ FirebaseExtensionsDemo/FirebaseExtensionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseExtensionsDemoApp.swift
+//  FirebaseExtensionsDemo
+//
+//  Created by Stefan Blos on 09.07.25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseExtensionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
